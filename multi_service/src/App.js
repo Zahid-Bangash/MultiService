8cb637a00861a6ad904fe2606fd1afd73251f364
Muhammad Zahid <101349002+Zahid-Bangash@users.noreply.github.com>
2023-11-10 01:38:57 +0500

    ...

---
+++ multi_service/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import DoneAllIcon from "@mui/icons-material/DoneAll";
+
+export default function App() {
+  return (
+    <div className="container d-flex my-5">
+      <h1 style={{ fontWeight: "600" }}>Multi Service Web App</h1>
+    </div>
+  );
+}
