2be724d79b3d07f977c58658a8028ffc520df3b8
Muhammad Zahid <101349002+Zahid-Bangash@users.noreply.github.com>
2023-11-20 02:16:34 +0500

    ...

--- multi_service/src/pages/Home.jsx
+++ multi_service/src/pages/Home.jsx
@@ -5,17 +5,31 @@ export default function Home() {
     <div className="home-page">
       <div className="hero" style={{ backgroundColor: "#F5F7FC" }}>
         <div className="container">
-          <div className="row">
+          <div className="row py-5">
             <div className="col-md-6">
-              <h1 className="mb-3" style={{ fontSize: "59px" }}>
+              <h1
+                className="mb-3"
+                style={{ fontSize: "59px", fontWeight: "600" }}
+              >
                 Discover & Book Local Service Providers
               </h1>
-              <div className="text-secondary" style={{ fontSize: "20px" }}>
+              <div
+                className="text-secondary"
+                style={{ fontSize: "20px", fontWeight: "400" }}
+              >
                 Search From 100 Awesome Verified Service Providers
               </div>
             </div>
           </div>
         </div>
+      </div>
+      <div className="py-5">
+        <h1
+          className="text-center"
+          style={{ fontWeight: "600", fontSize: "48px" }}
+        >
+          How It Works
+        </h1>
       </div>
     </div>
   );
